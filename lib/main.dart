f638054f66a1349e9a376c614cf6c5645fd642ae
Elvan Hidayat <elvanhd@gmail.com>
2024-04-02 01:39:45 +0700

    Introduce dark theme and enhance item display
    - Implement a dark theme variant with custom color schemes and apply it across UI elements to provide a cohesive appearance in dark mode
    - Customize CardTheme and ElevatedButtonTheme within the dark theme to ensure visual consistency with the light theme
    - The dark theme activation is prepared but commented out, allowing easy switch to system-based theme mode in the future
    - Adjust the display of expense amount in ExpenseItem for improved legibility by ensuring consistent formatting
    - These enhancements aim to improve user experience and accessibility across different system themes

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,30 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
@@ -34,6 +55,7 @@ void main() {
               ),
             ),
       ),
+      // themeMode: ThemeMode.system, // default
       home: const Expenses(),
     ),
   );
