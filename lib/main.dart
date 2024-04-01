1d9402c26dd3fe16daa20bbe6a09bdb7589580f6
Elvan Hidayat <elvanhd@gmail.com>
2024-04-02 01:06:31 +0700

    Enhance app theme and AppBar appearance
    - Define a custom color scheme with a specific seed color to enhance UI appearance
    - Update the ThemeData to incorporate the custom color scheme across the app
    - Customize AppBarTheme within ThemeData to align with the new color scheme
    - Set AppBar background color to onPrimaryContainer for a distinct look
    - Ensure AppBar text and icons (foregroundColor) contrast effectively with the background

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
       home: const Expenses(),
     ),
   );
