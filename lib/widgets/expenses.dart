588ffeaa766f5ac31434cd63fa9c6d4bfeb3c58e
Elvan Hidayat <elvanhd@gmail.com>
2024-04-01 23:58:35 +0700

    Enhance expense management with removal and UI adjustments
    - Enable scrolling for the modal bottom sheet in NewExpense widget
    - Implement _removeExpense method in Expenses widget to handle expense removal
    - Introduce Dismissible widget in ExpensesList for swipe-to-delete functionality
    - Automatically close the modal bottom sheet upon successful expense submission
    - Adjust padding for the NewExpense form when the keyboard is visible for better UX
    - Pass the _removeExpense callback to ExpensesList to facilitate expense deletion

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -30,6 +30,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     );
@@ -38,6 +39,12 @@ class _ExpensesState extends State<Expenses> {
   void _addExpense(Expense expense) {
     setState(() {
       _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
     });
   }
 
@@ -57,7 +64,10 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text('The chart'),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+              onRemoveExpense: _removeExpense,
+            ),
           ),
         ],
       ),
--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -66,6 +66,7 @@ class _NewExpenseState extends State<NewExpense> {
         category: _selectedCategory,
       ),
     );
+    Navigator.pop(context);
   }
 
   @override
@@ -78,7 +79,7 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
       child: Column(
         children: [
           TextField(
