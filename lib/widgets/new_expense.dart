a2b098bef8628d45b1c884b9629ab1ef407890b9
Elvan Hidayat <elvanhd@gmail.com>
2024-04-01 22:53:47 +0700

    Expand NewExpense form with amount input
    - Introduce _amountController for managing amount input field
    - Add a TextField for amount input with a dollar prefix and numeric keyboard
    - Include a dispose method for _amountController to clean up resources
    - Implement a Cancel button alongside the Save Expense button
    - Adjust the save button's onPressed to print both title and amount

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -11,10 +11,12 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -31,11 +33,24 @@ class _NewExpenseState extends State<NewExpense> {
               label: Text('Title'),
             ),
           ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: '\$ ',
+              label: Text('Amount'),
+            ),
+          ),
           Row(
             children: [
+              TextButton(
+                onPressed: () {},
+                child: const Text('Cancel'),
+              ),
               ElevatedButton(
                 onPressed: () {
                   print(_titleController.text);
+                  print(_amountController.text);
                 },
                 child: const Text('Save Expense'),
               ),
