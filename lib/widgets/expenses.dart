ded33e311d7e606e0ebace5e25d01d58b0ad72d8
Elvan Hidayat <elvanhd@gmail.com>
2024-04-02 19:16:29 +0700

    Introduce expense chart with dynamic categories
    - Create `Chart` widget to display a visual representation of expenses by category
    - Introduce `ChartBar` widget for individual category representation in the chart
    - Utilizes `fill` to dynamically adjust the bar height based on expense proportion
    - Integrate the `Chart` widget into the main `Expenses` widget, replacing the placeholder text
    - Utilize `categoryIcons` for visual category identification in the chart
    - Implement dark mode support for chart elements, adjusting colors accordingly
    - This update enhances user experience by providing a quick visual summary of expenses distributed across different categories

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/chart/chart.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
@@ -89,7 +90,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text('The chart'),
+          Chart(expenses: _registeredExpenses),
           Expanded(
             child: mainContent,
           ),
