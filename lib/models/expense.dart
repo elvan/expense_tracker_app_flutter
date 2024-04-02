eaee28df4b64660027c7a9fb62161203eecb3ec6
Elvan Hidayat <elvanhd@gmail.com>
2024-04-02 19:00:31 +0700

    Introduce expense categorization and aggregation
    - Import `uuid` library after `intl` for consistent organization
    - Add `ExpenseBucket` class to categorize and aggregate expenses by category
    - The constructor `ExpenseBucket.forCategory` creates an instance for a specific category, filtering the expenses accordingly
    - `totalExpenses` computes the total amount spent per category by iterating over all expenses in that category
    - This enhancement allows for more detailed analysis and reporting of expenses by category, aiding in better financial management and insight

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:uuid/uuid.dart';
 import 'package:intl/intl.dart';
+import 'package:uuid/uuid.dart';
 
 final formatter = DateFormat.yMd();
 
@@ -31,5 +31,30 @@ class Expense {
 
   String get formattedDate {
     return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount; // sum = sum + expense.amount
+    }
+
+    return sum;
   }
 }
